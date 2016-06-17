7ab7dcd9eddfae39298f7d6b07df012897b95351
jessicasolish <jessica.solis.h@gmail.com>
2016-06-17 10:19:16 -0300

    nuevo trabajo grupal de encontrar pares y mostrarlos en un arreglo ademas ejercicio dni

---
+++ Ejercicio1 -Calculando DNI/Problema/main.js
@@ -0,0 +1,51 @@
+  /*var numeros=[0,1,2,3,4,5,6,7,8,9,10];
+  console.log(numeros);
+  numeros.reverse();
+  var numero2=[];
+
+  for(var i=0;i <numeros.length;i++)
+  {
+  if(numeros[i] % 2==0){
+  numero2.push(numeros[i]); 
+  }
+  }
+  console.log(numeros2);
+
+  var dni = prompt("Introduce el número del DNI");
+  var letra = prompt("Indique la letra en mayusculas");
+  var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+  
+  // se comprueba que el número está dentro del rango
+  while ((dni<0)||(dni>99999999)){
+    dni = prompt("El número introducido no es correto (debe estar entre 0 y 99999999), vuelva a digitarlo");
+  }  
+
+  // se averigua el cociente del número proporcionado / 23
+  var num = dni % 23;
+  var letraReal = letras[num];  
+  
+  // mientras la letra no sea la correcta se seguirá pidiendo la letra correcta
+  
+  if (letra != letraReal){
+    while(letra != letraReal){    
+      letra = prompt("La letra es incorrecta, pruebe de nuevo");            
+    }
+    alert("La letra es correcta");
+  }    
+  else{
+     alert("La letra es correcta");
+  } 
+  */
+  var nombres =["jessica","alondra","javiera","carolina"];
+  console.log(nombres);
+  var nomb=[];
+
+  for(var i=0;i <nombres.length;i++)
+  {
+    nomb.push(nombres[i]);
+  }
+  console.log(nomb);
+
+
+
+
---
+++ Ejercicio2 -Añadir elementos/Problema/main.js
@@ -0,0 +1,18 @@
+var numeros=[0,1,2,3,4,5,6,7,8,9,10];
+console.log(numeros);
+numeros.reverse();
+var numero2=[];
+
+for(var i=0;i <numeros.length;i++)
+{
+	if(numeros[i] % 2==0){
+	numero2.push(numeros[i]);	
+	}
+}
+console.log(numeros2);
+
+
+
+
+
+
